79fda745519cfc1cd1eeb5905aeea8696786d3b8
TahirAtakanCan <atkan4202@gmail.com>
2023-11-01 21:32:13 +0300

    Initial Commit

---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Tahir Atakan Can on 1.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
